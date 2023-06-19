8690cd1a0dab81e7b615b4a7889b2a82c5eb07da
Huma <humazahoor786@gmail.com>
2023-06-19 15:51:18 +0500

    edit api changed

--- index.js
+++ index.js
@@ -3,32 +3,30 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("./models/user");
-const app = express();
-
 const mongoose = require("mongoose");
+
+const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
 // Connect to MongoDB
 mongoose
   .connect("mongodb://localhost:27017/myDatabase")
   .then((result) => {
     console.log("App running");
+    // create a login route and validate a user and send JWT token
     app.post("/login", async (req, res) => {
       const { username, password } = req.body;
-
       try {
-        // Find the user in the database
         const user = await User.findOne({ username });
         if (!user) {
           return res.status(404).json({ error: "User not found" });
         }
-        // Validate the password
         const validPassword = bcrypt.compare(password, user.password);
         if (!validPassword) {
           return res.status(401).json({ error: "Invalid password" });
         }
-        // Create a JWT token
         const token = jwt.sign({ userId: user._id }, "your_secret_key", {
           expiresIn: "1h",
         });
@@ -39,28 +37,28 @@ mongoose
       }
     });
 
+    // create a middleware function
     app.use((req, res, next) => {
       const token = req.headers.authorization;
       if (!token) {
         return res.status(401).json({ error: "Unauthorized" });
       }
-
       jwt.verify(token, "your_secret_key", (err, user) => {
         if (err) {
           return res.status(403).json({ error: "Forbidden" });
         }
-        // req.user = user;
         next();
       });
     });
 
+    // create a routes to add, edit or delete the user
     app.post("/add-user", (req, res) => {
       const newUser = new User(req.body);
       newUser
         .save()
         .then((result) => {
           res.json(result);
-          console.log("user created");
+          console.log("User has been created");
         })
         .catch((error) => {
           res.status(500).json({ error: "Internal server error" });
@@ -71,7 +69,7 @@ mongoose
       const userId = req.body.userId;
       User.findByIdAndRemove(userId)
         .then((result) => {
-          console.log("User removed");
+          console.log("User has been removed");
           res.json(result);
         })
         .catch((error) => {
@@ -80,15 +78,19 @@ mongoose
     });
 
     app.post("/edit-user", (req, res) => {
-      User.findById(req.body.id)
+      const userId = req.body.id;
+      const newUsername = req.body.username;
+      const newPassword = req.body.password;
+      const newEmail = req.body.email;
+      User.findById(userId)
         .then((user) => {
-          user.username = req.body.username;
-          user.password = req.body.password;
-          user.email = req.body.email;
+          user.username = newUsername;
+          user.password = newPassword;
+          user.email = newEmail;
           return user.save();
         })
         .then((result) => {
-          console.log("user Updated");
+          console.log("User has been Updated");
           res.json(result);
         })
         .catch((error) => {
@@ -96,6 +98,6 @@ mongoose
         });
     });
 
-    app.listen(3000);
+    app.listen(port);
   })
-  .catch((err) => console.log("Error is : ", err));
+  .catch((err) => console.log("Some error occured : ", err));
