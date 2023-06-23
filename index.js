220d9418ab5044785226349ddc26517aeb78ed3d
Huma <humazahoor786@gmail.com>
2023-06-23 19:02:35 +0500

    Some modifications done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("./models/user");
 const mongoose = require("mongoose");
+
+const verifyToken = require("./middleware");
 
 const app = express();
 const port = 3000;
@@ -36,68 +39,77 @@ mongoose
         res.status(500).json({ error: "Internal server error" });
       }
     });
-
-    // create a middleware function
-    app.use((req, res, next) => {
-      const token = req.headers.authorization;
-      if (!token) {
-        return res.status(401).json({ error: "Unauthorized" });
-      }
-      jwt.verify(token, "your_secret_key", (err, user) => {
-        if (err) {
-          return res.status(403).json({ error: "Forbidden" });
-        }
-        next();
-      });
-    });
-
     // create a routes to add, edit or delete the user
-    app.post("/add-user", (req, res) => {
-      const newUser = new User(req.body);
-      newUser
-        .save()
-        .then((result) => {
-          res.json(result);
-          console.log("User has been created");
-        })
-        .catch((error) => {
-          res.status(500).json({ error: "Internal server error" });
-        });
+    app.post("/add-user", verifyToken, async (req, res) => {
+      try {
+        const { email, password, username } = req.body;
+        if (!(email && password && username)) {
+          res.status(400).send("All input are required");
+        }
+        const user = await User.findOne({ email });
+        console.log("user", user);
+        if (user) {
+          return res.status(409).json({ error: "User Already Registered" });
+        } else {
+          console.log("here");
+          var encryptedPassword = await bcrypt.hash(password, 10);
+          console.log("----->", encryptedPassword);
+          // const user = await User.create({
+          //   username,
+          //   email: email.toLowerCase(), // sanitize: convert email to lowercase
+          //   password: encryptedPassword,
+          // });
+          // console.log(user);
+          const newUser = new User({
+            username,
+            email: email.toLowerCase(), // sanitize: convert email to lowercase
+            password: encryptedPassword,
+          });
+          newUser
+            .save()
+            .then((result) => {
+              res.json(result);
+              console.log("User has been created");
+            })
+            .catch((error) => {
+              res.status(500).json({ error: "Internal server error" });
+            });
+        }
+        res.status(201).json({ message: "User created Successfully " });
+      } catch (error) {
+        res.send("something went wrong");
+      }
     });
-
-    app.post("/delete-user", (req, res) => {
-      const userId = req.body.userId;
-      User.findByIdAndRemove(userId)
-        .then((result) => {
-          console.log("User has been removed");
-          res.json(result);
-        })
-        .catch((error) => {
-          res.status(500).json({ error: "Internal server error" });
-        });
+    app.delete("/delete/:userId", verifyToken, async (req, res) => {
+      const userId = req.params.userId;
+      try {
+        const deletedUser = await User.findByIdAndRemove(userId);
+        if (!deletedUser) {
+          return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully" });
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
     });
-
-    app.post("/edit-user", (req, res) => {
-      const userId = req.body.id;
-      const newUsername = req.body.username;
-      const newPassword = req.body.password;
-      const newEmail = req.body.email;
-      User.findById(userId)
-        .then((user) => {
-          user.username = newUsername;
-          user.password = newPassword;
-          user.email = newEmail;
-          return user.save();
-        })
-        .then((result) => {
-          console.log("User has been Updated");
-          res.json(result);
-        })
-        .catch((error) => {
-          res.status(500).json({ error: "Internal server error" });
-        });
+    app.put("/edit/:userId", verifyToken, async (req, res) => {
+      const userId = req.params.userId;
+      const { username, email, password } = req.body;
+      try {
+        const user = await User.findById(userId);
+        if (!user) {
+          return res.status(404).json({ error: "User not found" });
+        }
+        user.username = username || user.username;
+        user.email = email || user.email;
+        user.password = password || user.password;
+        // Save the updated user
+        await user.save();
+        res.status(200).json({ message: "User updated successfully" });
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
     });
-
     app.listen(port);
   })
-  .catch((err) => console.log("Some error occured : ", err));
+  .catch((err) => console.log("Some error occurred : ", err));
--- models/user.js
+++ models/user.js
@@ -2,9 +2,20 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const userSchema = new schema({
-  username: String,
-  email: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
